69aa4def6586161287dc11871f16a2f59d0801d0
Sanchit <sanchitgupta456@gmail.com>
2021-02-06 22:35:30 +0530

    Server Test with users

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+        "name": "Sanchit",
+        "team": "Karmac",
+        "points": 10
+    },
+    {
+        "name":"Gourav",
+        "team":"Karmac",
+        "points":5
+    }
+]
+
+export default data;
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import data from './data.js';
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -6,7 +9,9 @@ app.get('/', (req,res) => {
     res.send('Server is ready');
 });
 
-const port = process.env.PORT || 5000;
+app.get('/users', (req,res) => {
+    res.send(data);
+})
 
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${5000}`);
